0bb34e7bad534a98bad0e3bf923f7a61b622677f
Alexey <alexmail121212@gmail.com>
2023-09-29 17:09:05 +0300

    Ex 29 done

--- EX_29/Program.cs
+++ EX_29/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+int[] arr = new int[8];
+
+arr = EnterArray(8);
+
+Console.WriteLine();
+
+PrintArray(arr);
+
+// Methods
+
+int[] EnterArray(int length)
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива (целое число)");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0 | i == arr.Length - 1)
+        {
+            if (i == 0)
+            {
+                Console.Write($"[{arr[i]}");
+            }
+            else
+            {
+                Console.Write($", {arr[i]}]");
+            }
+        }
+        else
+        {
+            Console.Write($", {arr[i]}");
+        }
+
+    }
+}
