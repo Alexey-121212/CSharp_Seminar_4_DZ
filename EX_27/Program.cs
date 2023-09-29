efb693951ff4c82c0b3661f6c81933b56d3ab5bf
Alexey <alexmail121212@gmail.com>
2023-09-29 12:45:18 +0300

    Ex 27 done

--- EX_27/Program.cs
+++ EX_27/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11 82 -> 10 9012 -> 12
+
+int num;
+
+Console.Clear();
+
+Console.WriteLine("Введите целое число:");
+num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine($"Сумма цифр = {sumOfDigits(num)}");
+
+// Methods
+
+int sumOfDigits(int number)
+{
+    int summa = number % 10;
+
+    while ((number / 10) >= 1)
+    {
+        number /= 10;
+        summa += number % 10;
+    }
+    return summa;
+}
