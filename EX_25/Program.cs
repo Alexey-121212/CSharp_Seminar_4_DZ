07d9effee334caf1c0c8380a7f2a8bf21fd73c90
Alexey <alexmail121212@gmail.com>
2023-09-29 11:49:45 +0300

    Ex 25 done

--- EX_25/Program.cs
+++ EX_25/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵) 2, 4 -> 16
+
+int num;
+int degree;
+
+Console.Clear();
+
+Console.WriteLine("Введите целое число:");
+num = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Введите степень - натуральное число:");
+    degree = Convert.ToInt32(Console.ReadLine());
+} while (degree < 1);
+
+Console.WriteLine($"{num} в степени {degree} = {DoDegree(num, degree)}");
+
+
+// Methods
+
+int DoDegree(int number, int deg)
+{
+    int result = number;
+
+    for (int i = 2; i <= deg; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
